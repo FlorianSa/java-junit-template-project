81f824e8ff9c729eb93f56bb636baa413a44fe32
porsc <porsc@192.168.0.18>
2018-11-15 23:41:27 +0100

    Annuitate

--- src/uk/co/placona/helloWorld/HelloWorld.java
+++ src/uk/co/placona/helloWorld/HelloWorld.java
@@ -1,9 +1,37 @@
 package uk.co.placona.helloWorld;
-
+import java.util.Locale;
+	import java.util.Scanner;
 public class HelloWorld {
 
-	public String sayHello() {
-		return "Hello World6";
-	}
+	
 
-}
+		public static void main(String[] args) {
+		Scanner in = new Scanner(System.in).useLocale(Locale.ENGLISH);
+		double darlehen, zinssatz, annuitaet,laufzeit;
+		
+		//Eingeben & Ausgeben
+		System.out.println("Geben Sie den Darlehensbetrag ein!");
+		darlehen = in.nextDouble();
+		System.out.println("Geben Sie den Zinssatz (in %) ein!");
+		zinssatz = in.nextDouble();
+		zinssatz = zinssatz/100;
+		System.out.println("Laufzeit eingeben!");
+		laufzeit = in.nextDouble();
+		
+		//Rechnen
+		annuitaet=berechneannuitaet(darlehen, zinssatz, laufzeit);
+		
+		//Ausgabe
+		System.out.println(annuitaet);
+		
+
+		}
+		//Rechner
+		public static double berechneannuitaet(double darlehen, double zinssatz, double laufzeit) {
+			
+		
+		double annuitaet;
+		annuitaet = darlehen* ((Math.pow(1+zinssatz,laufzeit)*zinssatz)/(Math.pow(1+zinssatz,laufzeit)-1));
+		return annuitaet;
+		}
+	}
